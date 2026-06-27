6d4a033b9fa42916ac58c1084effbb043936fd71
cyber-lays <ilyesbenammar1@gmail.com>
2026-06-27 15:19:04 +0100

    etch_a_Sketch

--- game.js
+++ game.js
@@ -1,51 +1,39 @@
 
 
-const colr =["blue","orange", "green", "yellow", "black","pink","gray","Aqua","purple","gold"];
-let a= 16;
-const btn =document.getElementById("pop");
-    btn.addEventListener("click", () => {
-       a=prompt("Choose your size grid: ");
-       if(a != undefined && a != NaN){
+const colr = ["blue", "orange", "green", "yellow", "black", "pink", "gray", "Aqua", "purple", "gold"];
+let size;
+const btn = document.getElementById("pop");
+btn.addEventListener("click", () => {
+    size = prompt("Choose your size grid: ");
+    if (size != undefined && size != NaN) {
         container.replaceChildren();
-       }
-     carr = [];
-    for(let i=0 ; i<a; i++){
-        const div = document.createElement("div");
-        container.appendChild(div); 
-        for(let j=0 ; j<a; j++){
-            const divv = document.createElement("div");
-            div.appendChild(divv);  
-            carr.push(divv); 
-        }
     }
-    carr.forEach((div) => {
-    div.addEventListener("mouseenter", () => {
-        let n =Math.floor(Math.random()*colr.length);
-        div.style.backgroundColor=colr[n];
-        
-  });    
+CreateColoredCubes(size);
+
 });
-    });
 
 
 const container = document.querySelector(".container");
-let carr = [];
-for(let i=0 ; i<16; i++){
-    const div = document.createElement("div");
-    container.appendChild(div); 
-    for(let j=0 ; j<16; j++){
-        const divv = document.createElement("div");
-        div.appendChild(divv);  
-        carr.push(divv); 
+CreateColoredCubes(16);
+
+function CreateColoredCubes(size){
+
+    let carr = [];
+    for (let i = 0; i < size; i++) {
+        const div = document.createElement("div");
+        container.appendChild(div);
+        for (let j = 0; j < size; j++) {
+            const divv = document.createElement("div");
+            div.appendChild(divv);
+            carr.push(divv);
+        }
     }
+    carr.forEach((div) => {
+        div.addEventListener("mouseenter", () => {
+            let n = Math.floor(Math.random() * colr.length);
+            div.style.backgroundColor = colr[n];
+
+        });
+    });
 }
 
-
-
-carr.forEach((div) => {
-    div.addEventListener("mouseenter", () => {
-        let n =Math.floor(Math.random()*colr.length);
-        div.style.backgroundColor=colr[n];
-        
-  });    
-});
